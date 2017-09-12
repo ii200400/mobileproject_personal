72a4e8005d077372e6221f06c9adb89e44c16a58
ii200400 <ii200400@naver.com>
2017-09-12 13:21:53 +0900

    second

---
+++ app/src/main/java/com/example/im/mobileproject/RegistrationPart.kt
@@ -0,0 +1,35 @@
+package com.example.im.mobileproject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+
+class RegistrationPart: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        val spin1: Spinner = findViewById(R.id.spinner1)
+        val text1: TextView = findViewById(R.id.textView1)
+
+        val value = arrayListOf("아아~","아~아아ㅏ","마이크 테스트")
+
+        spin1.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,value)
+
+        spin1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                text1.text = "선택하시오."
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                text1.text = value.get(p2)
+            }
+
+        }
+    }
+}
