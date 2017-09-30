c35032fe7d1b67aaa736481a3c09ed4ba8920954
JHan <dudgus9727@naver.com>
2017-09-30 13:19:31 +0900

    0930 trying AR

--- pcAssemblyHelper/app/src/main/java/com/vuforia/samples/VuforiaSamples/ARmain.kt
+++ pcAssemblyHelper/app/src/main/java/com/vuforia/samples/VuforiaSamples/ARmain.kt
@@ -4,19 +4,35 @@ import android.content.pm.ActivityInfo
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.GestureDetector
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
+import android.view.MotionEvent
 import java.util.*
+
+import com.vuforia.CameraDevice.FOCUS_MODE
+import com.vuforia.CameraDevice.FOCUS_MODE.FOCUS_MODE_TRIGGERAUTO
+import com.vuforia.CameraDevice
 
 // 나중에 지워야함
 import com.vuforia.samples.SampleApplication.SampleApplicationSession
 import com.vuforia.samples.SampleApplication.utils.LoadingDialogHandler
 import com.vuforia.samples.SampleApplication.utils.Texture
+import com.vuforia.samples.SampleApplication.SampleApplicationException
+import com.vuforia.samples.SampleApplication.utils.SampleApplicationGLView
+
 
 // AR 구현할 클래스
 class ARmain : AppCompatActivity() {
+
+    lateinit var vuforiaAppSession : SampleApplicationSession
+
+    var mIsDroidDevice : Boolean = false
+
+    lateinit var mGlView: SampleApplicationGLView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +41,7 @@ class ARmain : AppCompatActivity() {
         // 여기부터가 startLoadingAnimation 부분
         // SampleApplicationControl을 implement해야지 빨간줄 사라진다.
         // 추후 재설계 예정
-        var vuforiaAppSession = SampleApplicationSession(this)
+        vuforiaAppSession = SampleApplicationSession(this)
 
         // AR에 사용할 카메라용 레이아웃 설정
         var mUILayout = View.inflate(this, R.layout.camera_overlay,null) as RelativeLayout
@@ -74,6 +90,55 @@ class ARmain : AppCompatActivity() {
         mTextures.add(Texture.loadTextureFromApk("ImageTargets/Buildings.jpeg", assets))
 
         // 좀더 확인해야하는 부분
-        var mIsDroidDevice : Boolean = android.os.Build.MODEL.toLowerCase().startsWith("droid")
+        mIsDroidDevice = android.os.Build.MODEL.toLowerCase().startsWith("droid")
+    }
+
+    // Called when the activity will start interacting with the user.
+    override fun onResume() {
+        Log.d("ARmain", "onResume")
+        super.onResume()
+
+        // This is needed for some Droid devices to force portrait
+        if (mIsDroidDevice) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
+        try {
+            vuforiaAppSession.resumeAR()
+        } catch (e: SampleApplicationException) {
+            Log.e("ARmain", e.string)
+        }
+
+        // Resume the GL view:
+        if (mGlView != null) {
+            mGlView.setVisibility(View.VISIBLE)
+            mGlView.onResume()
+        }
+
+    }
+}
+
+// 추후 재설계해야할 클래스
+// Process Single Tap event to trigger autofocus
+private class GestureListener : GestureDetector.SimpleOnGestureListener() {
+    // Used to set autofocus one second after a manual focus is triggered
+    private val autofocusHandler = Handler()
+
+    override fun onDown(e: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        // Generates a Handler to trigger autofocus
+        // after 1 second
+        autofocusHandler.postDelayed(Runnable {
+            val result = CameraDevice.getInstance().setFocusMode(CameraDevice.FOCUS_MODE.FOCUS_MODE_TRIGGERAUTO)
+
+            if (!result)
+                Log.e("SingleTapUp", "Unable to trigger focus")
+        }, 1000L)
+
+        return true
     }
 }
