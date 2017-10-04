930ce57aba7e098c17d369f6fc063e9662762958
kitoha <kth004@naver.com>
2017-10-05 06:04:26 +0900

    add activity

--- app/src/main/java/com/example/im/mobileproject/Assembly.kt
+++ app/src/main/java/com/example/im/mobileproject/Assembly.kt
@@ -1,17 +1,13 @@
 package com.example.im.mobileproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
 
 class Assembly : AppCompatActivity() {
-
-    var fragment1 : Fragment1=Fragment1()
-    var fragment2 : Fragment2=Fragment2()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +16,21 @@ class Assembly : AppCompatActivity() {
 
         val spin1: Spinner = findViewById(R.id.spinner)
         val spin2: Spinner = findViewById(R.id.spinner2)
-
+        val bnt:Button=findViewById(R.id.button)
+        val bnt2:Button=findViewById(R.id.button2)
         val category= arrayOf("카테고리","메인보드","CPU","RAM","메모리")
 
         spin2.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,category)
-        fragmentManager.beginTransaction().replace(R.id.container,fragment1).commit()
+
+        bnt.setOnClickListener {
+
+        }
+
+        bnt2.setOnClickListener {
+            val intent1:Intent=Intent(this@Assembly,Assembly_Register::class.java)
+            startActivity(intent1)
+        }
+
     }
 
-    fun onFragmentChange(index: Int){
-        var index=index
-
-        if(index==0){
-            fragmentManager.beginTransaction().replace(R.id.container,fragment1).commit()
-        }
-        else if(index==1){
-            fragmentManager.beginTransaction().replace(R.id.container,fragment2).commit()
-        }
-    }
 }
