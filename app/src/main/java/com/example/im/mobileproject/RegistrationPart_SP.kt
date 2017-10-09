2da3ea90f4fe4c5e0e234d0f939069e3f11378a1
ii200400 <ii200400@naver.com>
2017-10-09 18:29:52 +0900

    코드 이해중

--- app/src/main/java/com/example/im/mobileproject/RegistrationPart_SP.kt
+++ app/src/main/java/com/example/im/mobileproject/RegistrationPart_SP.kt
@@ -2,6 +2,7 @@
 package com.example.im.mobileproject
 
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
@@ -55,12 +56,6 @@ class RegistrationPart_SP : AppCompatActivity() {
     //private val mFlashSupported: Boolean = false ..?왜 있을까;
     private var mBackgroundHandler: Handler? = null
     private var mBackgroundThread: HandlerThread? = null
-
-    var values : ContentValues? = null
-    var values2 : ContentValues? = null
-    var url : Uri? = null
-    var url2 : Uri? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -196,17 +191,17 @@ class RegistrationPart_SP : AppCompatActivity() {
                     var bitmap : Bitmap? = null
                     try{
                         //TODO 갤러리에서 이미지 볼 수 있도록 하기
-                        values = ContentValues()
-                        values!!.put(MediaStore.Images.Media.TITLE, "사진1");
-                        values!!.put(MediaStore.Images.Media.DISPLAY_NAME, "사진2");
-                        values!!.put(MediaStore.Images.Media.DESCRIPTION, "제발..");
-                        values!!.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                        val values = ContentValues()
+                        values.put(MediaStore.Images.Media.TITLE, "사진1");
+                        values.put(MediaStore.Images.Media.DISPLAY_NAME, "사진2");
+                        values.put(MediaStore.Images.Media.DESCRIPTION, "제발..");
+                        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
 
                         //갤러리의 상단에 넣어주기
-                        values!!.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
-                        values!!.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+                        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
 
-                        url = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                        val url = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
                         image = reader.acquireLatestImage()
 
                         if(image != null) {
@@ -220,6 +215,11 @@ class RegistrationPart_SP : AppCompatActivity() {
                             bitmap.copyPixelsFromBuffer(buffer)
                             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, imageOut)
 
+                            //val input : InputStream = contentResolver.openInputStream(url)
+                            //val inputBitmap : Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, url)
+                            //pickedImage.setImageBitmap(inputBitmap)
+
+
                             val id : Long = ContentUris.parseId(url)
                             val miniThumb = MediaStore.Images.Thumbnails.getThumbnail(contentResolver, id, MediaStore.Images.Thumbnails.MINI_KIND, null)
 
@@ -227,16 +227,17 @@ class RegistrationPart_SP : AppCompatActivity() {
                             matrix.setScale(50F / miniThumb.width, 50F / image.height)
 
                             val thumb : Bitmap = Bitmap.createBitmap(miniThumb, 0, 0, miniThumb.width, miniThumb.height, matrix, true)
-                            values2 = ContentValues(4)
-                            values2!!.put(MediaStore.Images.Thumbnails.KIND, MediaStore.Images.Thumbnails.MICRO_KIND)
-                            values2!!.put(MediaStore.Images.Thumbnails.IMAGE_ID, id.toInt())
-                            values2!!.put(MediaStore.Images.Thumbnails.HEIGHT, thumb.height)
-                            values2!!.put(MediaStore.Images.Thumbnails.WIDTH, thumb.width)
+                            val values2 = ContentValues(4)
+                            values2.put(MediaStore.Images.Thumbnails.KIND, MediaStore.Images.Thumbnails.MICRO_KIND)
+                            values2.put(MediaStore.Images.Thumbnails.IMAGE_ID, id.toInt())
+                            values2.put(MediaStore.Images.Thumbnails.HEIGHT, thumb.height)
+                            values2.put(MediaStore.Images.Thumbnails.WIDTH, thumb.width)
 
-                            url2 = contentResolver.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values2)
+                            val url2 = contentResolver.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values2)
                             val thumbOut : OutputStream = contentResolver.openOutputStream(url2)
                             thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut)
-                            thumbOut.close();
+                            thumbOut.close()
+
                         }
                     }catch (e : FileNotFoundException){
                         e.printStackTrace()
@@ -260,23 +261,6 @@ class RegistrationPart_SP : AppCompatActivity() {
                     super.onCaptureCompleted(session, request, result)
                     Toast.makeText(this@RegistrationPart_SP, "Saved:" + file, Toast.LENGTH_SHORT).show()
                     createCameraPreview()
-
-                    Log.e("----------",url.toString())
-                    if (url!=null){
-                        /*
-                        val input : InputStream = this@RegistrationPart_SP.contentResolver.openInputStream(url)
-                        val bitmapOptions : BitmapFactory.Options = BitmapFactory.Options()
-                        bitmapOptions.inJustDecodeBounds = true
-                        bitmapOptions.inPreferredConfig=Bitmap.Config.ARGB_8888
-                        val inputBitmap : Bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions)
-                        pickedImage.setImageBitmap(inputBitmap)
-                        input.close()
-                        */
-                        val inputBitmap : Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, url)
-                        Log.e("----------",inputBitmap.toString())
-                        pickedImage.setImageURI(null)
-                        pickedImage.setImageURI(url)
-                    }
                 }
             }
             //매개변수 3개인 함수 불러온 것
