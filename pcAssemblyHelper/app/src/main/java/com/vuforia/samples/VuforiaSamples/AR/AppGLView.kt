b72653762331d16232e8588f0a20f518b17ee41d
JHan <dudgus9727@naver.com>
2017-10-15 20:32:05 +0900

    1015 trying openGL

--- pcAssemblyHelper/app/src/main/java/com/vuforia/samples/VuforiaSamples/AR/AppGLView.kt
+++ pcAssemblyHelper/app/src/main/java/com/vuforia/samples/VuforiaSamples/AR/AppGLView.kt
@@ -64,10 +64,10 @@ class AppGLView(context : Context) : GLSurfaceView(context)
     // openGL 에러 체크하는 함수
     fun checkEglError(prompt : String, egl : EGL10)
     {
-        var error : Int
+        var error : Int = egl.eglGetError()
 
         // 에러를 확인하고 해당하는 에러를 로그로 출력한다.
-        while ((error = egl.eglGetError()) != EGL10.EGL_SUCCESS)
+        if (error != EGL10.EGL_SUCCESS)
         {
             Log.e("AppGLView", String.format("%s: EGL error: 0x%x", prompt, error))
         }
@@ -76,7 +76,7 @@ class AppGLView(context : Context) : GLSurfaceView(context)
     // Creates OpenGL contexts.
     // AppGLView 클래스의 함수를 사용하기 편리하게 inner 클래스로 선언
     // openGL의 context를 생성한다.
-    private inner class ContextFactory : GLSurfaceView.EGLContextFactory
+    inner class ContextFactory : GLSurfaceView.EGLContextFactory
     {
         // EGL context의 클라이언트 버전
         private val EGL_CONTEXT_CLIENT_VERSION = 0x3098
@@ -115,4 +115,101 @@ class AppGLView(context : Context) : GLSurfaceView(context)
         }
     }
 
+    // The config chooser.
+    // openGL 사용을 위해 설정을 선택하는 클래스
+    // 생성자로 RGB와 알파값, depth값, 개체들간의 구분을 해주는 stencil 크기를 입력받는다.
+    inner class ConfigChooser(var mRedSize : Int, var mGreenSize : Int, var mBlueSize : Int,
+                              var mAlphaSize : Int, var mDepthSize : Int, var mStencilSize : Int)
+        : GLSurfaceView.EGLConfigChooser
+    {
+        var mValue = IntArray(1)
+
+        // 반드시 오버라이드 해야하는 함수
+        // 설정을 선택하는 함수
+        override fun chooseConfig(egl: EGL10?, display: EGLDisplay?): EGLConfig?
+        {
+            // This EGL config specification is used to specify 2.0
+            // rendering. We use a minimum size of 4 bits for
+            // red/green/blue, but will perform actual matching in
+            // chooseConfig() below.
+            val EGL_OPENGL_ES2_BIT = 0x0004
+
+            // egl 설정을 위한 배열 설정
+            val s_configAttribs_gl20 = intArrayOf(EGL10.EGL_RED_SIZE, 4, EGL10.EGL_GREEN_SIZE, 4,
+                    EGL10.EGL_BLUE_SIZE, 4, EGL10.EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL10.EGL_NONE)
+
+            // 위에서 만든 배열을 egl 설정에 맞게 포멧을 맞춰주는 함수를 리턴
+            return getMatchingConfig(egl, display, s_configAttribs_gl20)
+        }
+
+        // 위 함수에서 인자만 다른 오버로딩된 함수
+        fun chooseConfig(egl: EGL10?, display: EGLDisplay?,
+                         configs: Array<EGLConfig?>): EGLConfig?
+        {
+            // egl 설정이 담긴 배열의 원소를 하나씩 반복
+            for (config in configs)
+            {
+                val d = findConfigAttrib(egl!!, display!!, config!!,
+                        EGL10.EGL_DEPTH_SIZE, 0)
+                val s = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_STENCIL_SIZE, 0)
+
+                // We need at least mDepthSize and mStencilSize bits
+                // depth와 stencil 크기가 최소인 값을 찾는다.
+                if (d < mDepthSize || s < mStencilSize)
+                    continue
+
+                // We want an *exact* match for red/green/blue/alpha
+                // 정확한 RGB와 알파값을 찾는다.
+                val r = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_RED_SIZE, 0)
+                val g = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_GREEN_SIZE, 0)
+                val b = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_BLUE_SIZE, 0)
+                val a = findConfigAttrib(egl, display, config,
+                        EGL10.EGL_ALPHA_SIZE, 0)
+
+                // 정확한 값이면 설정 리턴
+                if (r == mRedSize && g == mGreenSize && b == mBlueSize
+                        && a == mAlphaSize)
+                    return config
+            }
+
+            // 해당하는 설정 값이 없으면 null 리턴
+            return null
+        }
+
+        // 배열을 egl 설정 포맷에 맞게 맞춰주는 함수
+        fun getMatchingConfig(egl: EGL10?, display: EGLDisplay?,
+                                      configAttribs: IntArray) : EGLConfig?
+        {
+            // Get the number of minimally matching EGL configurations
+            // egl 설정과 맞는 숫자를 얻는다.
+            val num_config = IntArray(1)
+            egl!!.eglChooseConfig(display, configAttribs, null, 0, num_config)
+
+            // egl 설정과 맞는 설정이 없으면 오류 메세지 출력
+            val numConfigs = num_config[0]
+            if (numConfigs <= 0)
+                throw IllegalArgumentException("No matching EGL configs")
+
+            // Allocate then read the array of minimally matching EGL configs
+            // 맞는 egl 설정을 배열에 넣고 읽는다.
+            val configs = arrayOfNulls<EGLConfig>(numConfigs)
+            egl.eglChooseConfig(display, configAttribs, configs, numConfigs,
+                    num_config)
+
+            // Now return the "best" one
+            // 포맷을 맞춘 egl 설정을 리턴한다.
+            return chooseConfig(egl, display, configs)
+        }
+
+        // 인자로 넘겨받은 설정 중 맞는 걸 찾아서 리턴해주는 함수
+        fun findConfigAttrib(egl: EGL10, display: EGLDisplay,
+                                     config: EGLConfig, attribute: Int, defaultValue: Int): Int
+        {
+            return if (egl.eglGetConfigAttrib(display, config, attribute, mValue)) mValue[0] else defaultValue
+        }
+    }
 }
