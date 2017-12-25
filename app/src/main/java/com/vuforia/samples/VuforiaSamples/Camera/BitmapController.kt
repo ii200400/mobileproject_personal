44e035e598b546e0db669e4f4f74f54fa6e2981b
ii200400 <ii200400@naver.com>
2017-12-25 15:01:55 +0900

    머지 전에 커밋

---
+++ app/src/main/java/com/vuforia/samples/VuforiaSamples/Camera/BitmapController.kt
@@ -0,0 +1,27 @@
+package com.vuforia.samples.VuforiaSamples.Camera
+
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+
+
+
+/**
+ * Created by im on 2017-12-25.
+ */
+class BitmapController {
+//    fun smallerBitmap(res: uri, resId: Int,
+//                                        reqWidth: Int, reqHeight: Int): Bitmap {
+//
+//        // First decode with inJustDecodeBounds=true to check dimensions
+//        val options = BitmapFactory.Options()
+//        options.inJustDecodeBounds = true
+//        BitmapFactory.decodeResource(res, resId, options)
+//
+//        // Calculate inSampleSize
+//        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+//
+//        // Decode bitmap with inSampleSize set
+//        options.inJustDecodeBounds = false
+//        return BitmapFactory.decodeResource(res, resId, options)
+//    }
+}
